7c691f7254876417b87ee84623b3ebafcc60f8ca
keipro00 <keipro00@outlook.jp>
2020-11-04 15:38:55 +0900

    リセットCSSの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # ①rootはどのようなときに実行される？、②コントローラーとビューを作成するがmessagesコントローラーはターミナルで作成する、Explorerから作成してもうよいの？
+  root to: "messages#index"
+
 end
