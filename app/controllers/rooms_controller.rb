1d41d6617ea4548dab697f887c269446d0fb105b
keipro00 <keipro00@outlook.jp>
2020-11-06 12:48:30 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,19 @@ class RoomsController < ApplicationController
     # インスタンス変数を入れる意味、users_controllerとの違い
     @room = Room.new
   end
+
+  def create
+    # binding.pry
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+end
+private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
